09a6f39b34acd392d703ef4ea7e56e758a262843
gerardoceja <89027066+gerardoceja@users.noreply.github.com>
2021-08-23 08:20:08 -0700

    Update exercise.py

--- assignments/01Ejercicio/src/exercise.py
+++ assignments/01Ejercicio/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    h=int(input("Dame las horas"))
+    m=int(input("Dame los minutos"))
+    s=int(input("Dame los segundos"))
+    h=h*3600
+    m=m*60
+    print(h+m+s)
 
 if __name__ == '__main__':
     main()
