9f4fc3ea4bb1807a47bab2dfa27e6690467f9f07
gerardoceja <89027066+gerardoceja@users.noreply.github.com>
2021-08-23 14:27:19 -0700

    Update exercise.py

--- assignments/01Ejercicio/src/exercise.py
+++ assignments/01Ejercicio/src/exercise.py
@@ -1,6 +1,25 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    def equiv (h,m,s):
+       st = h*3600+m*60+s
+       return st
+
+    print("proceso 1:")
+    h =int(input("Introduce las horas"))
+    m = int(input("Introduce las minutos"))
+    s = int(input("Introduce las segundos"))
+
+    eq1 = equiv(h,m,s)
+
+    print("proceso 2;")
+    h =int(input("Introduce las horas"))
+    m = int(input("Introduce las minutos"))
+    s = int(input("Introduce las segundos"))
+
+    eq2 = equiv(h,m,s)
+
+    print("proceso 1:", eq1)
+    print("proceso 2:", eq2)
 
 if __name__ == '__main__':
     main()
