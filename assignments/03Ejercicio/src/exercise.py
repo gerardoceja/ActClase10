12d8b8e9d453629f33818d439831118a211822f1
gerardoceja <89027066+gerardoceja@users.noreply.github.com>
2021-08-26 19:56:41 -0700

    Update exercise.py

--- assignments/03Ejercicio/src/exercise.py
+++ assignments/03Ejercicio/src/exercise.py
@@ -1,6 +1,25 @@
 def main() :
    #introduce el codigo
-   pass
+   def operaciones(num1,num2,letra):
+       if letra== "s":
+           suma= num1+num2
+           return suma
+       elif letra== "r":
+           resta= num1-num2
+           return resta
+       elif letra== "m":
+           multi= num1*num2
+           return multi
+       elif letra== "d":
+           divi= num1/num2
+           return divi
+   def main() :
+           tob=int(input("introudcir valor 1: "))
+           andr=int(input("introudcir valor 2: "))
+           tom=input("introducir clave  (s, r, m, d): ")
+           calculo= operaciones(tob,andr,tom)
+           print("resultado:",calculo)
+        
 
 
 
