f9bce3741b9033c5740adc3f5f5399ae79900b87
gerardoceja <89027066+gerardoceja@users.noreply.github.com>
2021-08-26 20:13:48 -0700

    Update exercise.py

--- assignments/01Ejercicio/src/exercise.py
+++ assignments/01Ejercicio/src/exercise.py
@@ -1,6 +1,25 @@
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    def equivalente(hr,mn,sg):
+        st = hr*3600+mn*60+sg
+        return st
+
+    print("proceso 1")
+    h = int(input("Introduce las horas: "))
+    m = int(input("Introduce las minutos: "))
+    s = int(input("Introduce las segundos: "))
+
+    eq1 = equivalente(h,m,s)
+
+    print("proceso 2:")
+    h = int(input("Introduce las horas: "))
+    m = int(input("Introduce las minutos: "))
+    s = int(input("Introduce las segundos: "))
+
+    eq2 = equivalente(h,m,s)
+
+    print("proceso 1:", eq1)
+    print("proceso 2:", eq2)
 
 if __name__ == '__main__':
     main()
