66b660a9db502903bf39435c6b6b6a4d427a1cd2
gerardoceja <89027066+gerardoceja@users.noreply.github.com>
2021-08-26 20:04:52 -0700

    Update exercise.py

--- assignments/02Ejercicio/src/exercise.py
+++ assignments/02Ejercicio/src/exercise.py
@@ -1,6 +1,22 @@
 def main() :
    #introduce el codigo
-   pass
+   def areaRect(a,1):
+       area=1*a
+       return area
+
+   def perimetroRect(a,1):
+       perimetro=(1*2)+(a*2)
+       return perimetro
+   def main() :
+       x=float(input("Ingresa el ancho: "))
+       y=float(input("Ingresa el largo: "))
+       print("Quiere calcular el area o el perimetro del rectangulo:")
+       print("(a para area y p para perimetro)")
+       agnes=input("op: ")
+
+       if agnes== "p":
+           pietro=perimetroRect(x,y)
+           print("perimetro",pietro)
 
 if __name__=='__main__':
     main()
